4e0db3cd3258650e30438bf659626415785e65f0
GenioFalco <GenioFalco@users.noreply.github.com>
2026-07-17 07:34:10 +0000

    deploy: 028e54eac344968529c56448c45c0ada194fa410

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4150679555" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1362330069" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
